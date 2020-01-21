f380b455687b4b2d37e077f112db1062a115d692
burhanelgun <belgun@gtu.edu.tr>
2020-01-21 15:21:29 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,75 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'notification-frontend';
+  jobName :string;
+  jobGroup:string;
+  triggerName:string;
+  triggerGroup:string;
+  cronExpression:string;
+  interval:string;
+
+
+  constructor(private httpClient: HttpClient,private router: Router) {
+
+  }
+
+  ngOnInit() {
+  }
+
+  createCronJob():void {
+
+    const formData = new FormData();
+    formData.append('jobName', this.jobName);
+    formData.append('jobGroup', this.jobGroup);
+    formData.append('triggerName', this.triggerName);
+    formData.append('triggerGroup', this.triggerGroup);
+    formData.append('cronExpression', this.cronExpression);
+
+     this.httpClient.post(`http://localhost:8080/api/startNotificationCron`,formData).subscribe(
+        (data:any)=>{
+          if(data=="Signed in"){
+            this.router.navigate(['create-new-job']);
+
+
+          }
+          else{
+            this.router.navigate(['joblist']);
+          }
+
+
+        });
+
+
+    }
+
+    createSimpleJob():void {
+
+      const formData = new FormData();
+      formData.append('jobName', this.jobName);
+      formData.append('jobGroup', this.jobGroup);
+      formData.append('triggerName', this.triggerName);
+      formData.append('triggerGroup', this.triggerGroup);
+      formData.append('interval', this.interval);
+
+       this.httpClient.post(`http://localhost:8080/api/startNotificationSimple`,formData).subscribe(
+          (data:any)=>{
+            if(data=="Signed in"){
+              this.router.navigate(['create-new-job']);
+
+
+            }
+            else{
+              this.router.navigate(['joblist']);
+            }
+
+
+          });
+
+
+      }
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +18,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    RouterModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
